881d14b8dc7d61ea14b4f669ad3e51d082c84917
Lavanyareads <lavanyanimbalkar06@gmail.com>
2026-08-17 22:39:46 +0530

    Updated the model because of error in previous one

--- groq_helper.py
+++ groq_helper.py
@@ -47,7 +47,7 @@ ISL gloss input: {text}
 """
 
     response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
+        model="openai/gpt-oss-20b",
         messages=[{"role": "user", "content": prompt}]
     )
 
